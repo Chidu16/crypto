2dec23e296f4cc5bbaf0838dfea98f38220ac7b9
Chidambar Gokak <x18104291@student.ncirl.ie>
2019-04-14 09:54:53 +0000

    twitter  key

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -4,8 +4,8 @@ class CryptosController < ApplicationController
   
   def show
       tweets = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV.fetch("TWITTER_KEY")
-      config.consumer_secret     = ENV.fetch("TWITTER_SECRET")
+      config.consumer_key        = 'F9ljVNOFrEVqmDQ6Ho4gxTaMa' 
+      config.consumer_secret     = 'MA2XgzTQvRkWTH5kTnOKMsVoQv6g7d1v3V2mM9ft9dW1LdjwE7'
     end
     @tweets = tweets.search('#cryptocurrencies').take(3)
   end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV.fetch("TWITTER_KEY"), ENV.fetch("TWITTER_SECRET")
+  provider :twitter, 'F9ljVNOFrEVqmDQ6Ho4gxTaMa', 'MA2XgzTQvRkWTH5kTnOKMsVoQv6g7d1v3V2mM9ft9dW1LdjwE7'
 end
