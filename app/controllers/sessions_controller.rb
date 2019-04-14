efee6a7ee5332a88ffdbe7c8fd6a5c5d8fa06179
Chidambar Gokak <x18104291@student.ncirl.ie>
2019-04-14 11:27:04 +0000

    login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     session[:user_id] = @user.id
-    redirect_to root_path
+    redirect_to '/cryptos/show'
   end
   
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   
  
+  root 'welcome#index'
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 match 'auth/failure', to: redirect("/") , via: [:get, :post]
 match 'signout', to: 'sessions#destroy', via: [:get, :post]
 
-  
-  root "cryptos#show", id: "bitcoin"
-  
-  resources :cryptos, only: :show
+
+resources :cryptos, only: :show
   
 end
