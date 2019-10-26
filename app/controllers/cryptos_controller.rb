c57d79e6f4d872bfb734e61aa7e4f40ce6762e47
Chidu16 <43790662+Chidu16@users.noreply.github.com>
2019-10-26 13:45:03 +0100

    removing keys

--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -5,12 +5,12 @@ class CryptosController < ApplicationController
   
   def show
       tweets = Twitter::REST::Client.new do |config|
-      config.consumer_key        = 'F9ljVNOFrEVqmDQ6Ho4gxTaMa' 
-      config.consumer_secret     = 'MA2XgzTQvRkWTH5kTnOKMsVoQv6g7d1v3V2mM9ft9dW1LdjwE7'
+      config.consumer_key        = 'Twitter Key' 
+      config.consumer_secret     = 'Twitter Secret'
     end
     @tweets = tweets.search('#cryptocurrencies').take(5)
     
-     newsapi = News.new("6554a6602f9f4517a27a73333a23ce95")
+     newsapi = News.new("News API Key")
     @top_headlines = newsapi.get_top_headlines(q: 'bitcoin').take(6)
   end
 
